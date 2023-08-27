05645ffcb4d9110739008472b6751478c8a0e229
Manoj Saini <manojsaini513@gmail.com>
2023-08-27 09:32:24 +0000

    done 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import Child from "./Child";
+import "./../styles/App.css";
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
 
-export default App
+  const [modal, showModal] = useState(false);
+  const updateParent = () => {
+    showModal(true);
+  };
+  
+  return (
+    <div className="parent">
+      {/* Do not remove the main div */}
+      <h1>Parent Component</h1>
+      <Child updateParent={updateParent} modal={modal} />
+    </div>
+  );
+};
+
+export default App;
